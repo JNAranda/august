1353f7837f114bc22676e817cb450be55f72da34
JNAranda <joearanda@gmail.com>
2013-04-17 07:14:02 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+August::Application.config.secret_token = 'a5b4a1481fd70480adfe34e57fbd12085677aa72b923d2df160ef5c772892bf57a56b5dd32a04f8a8076e310feed0477864c4a940751fbaccca21b6554b564d9'
